6dae5b9883ba9ebf15722a1c214b66b166db1627
Luiz <luizcb1944@hotmail.com>
2023-07-20 19:00:16 -0300

    Created metod get

--- src/main/java/com/products/api/controller/ProductController.java
+++ src/main/java/com/products/api/controller/ProductController.java
@@ -1,9 +1,21 @@
 package com.products.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.products.api.dto.ProductRecordDto;
+import com.products.api.models.ProductModel;
 import com.products.api.repositories.ProductRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class ProductController {
@@ -11,7 +23,15 @@ public class ProductController {
 	@Autowired
 	ProductRepository productRepository;
 	
+	@PostMapping("/products")
+	public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+		var productModel = new ProductModel();
+		BeanUtils.copyProperties(productRecordDto, productModel);
+		return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+	}
 	
-	
-
+	@GetMapping("/products")
+	public ResponseEntity<List<ProductModel>>getAllProducts(){
+		return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+	}
 }
