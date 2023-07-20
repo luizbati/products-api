f273701d3a8477889b502466d4d5917ac6bb6d2a
Luiz <luizcb1944@hotmail.com>
2023-07-20 19:12:42 -0300

    created put

--- src/main/java/com/products/api/controller/ProductController.java
+++ src/main/java/com/products/api/controller/ProductController.java
@@ -1,12 +1,15 @@
 package com.products.api.controller;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +36,14 @@ public class ProductController {
 	@GetMapping("/products")
 	public ResponseEntity<List<ProductModel>>getAllProducts(){
 		return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+	}
+	
+	@GetMapping("/products/{id}")
+	public ResponseEntity<Object> getOneProduct(@PathVariable(value="id") UUID id){
+		Optional<ProductModel> productO = productRepository.findById(id);
+		if(productO.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(productO.get());
 	}
 }
